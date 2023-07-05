00605fbb34ac45757b1254cde639e6f45191f589
Graham Kelly <gkgoat6700@gmail.com>
2023-07-04 22:52:06 -0400

    obfuscated successfully

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@ char *str2 = "bananas\n";
 int main() {
   volatile int x = 1;
 	volatile int y = 2;
-	while(y - 1 == x){
+	if(y - 1 == x){
 	printf("hello world %d\n", (x+y)*(x+y));
   char* newStr = strdup(str1);
 	strcat(newStr, str2);
